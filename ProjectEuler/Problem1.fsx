4eebd7ca82b488c3e016e0f31e94df65bf2b3924
Brian Gardner <brian.gardner@socraticdev.com>
2016-01-31 20:58:33 -0500

    Initial Commit.  First 2 problems solved.

---
+++ ProjectEuler/Problem1.fsx
@@ -0,0 +1,9 @@
+﻿let numbers = [0..999]
+
+let sumMultipleThreesAndFives (list:List<int>) = 
+    list
+    |> List.filter (fun x -> (x % 3 = 0) || (x % 5 = 0))
+    |> List.sum
+
+let answer = sumMultipleThreesAndFives numbers
+printfn "%A" answer
---
+++ ProjectEuler/Problem2.fsx
@@ -0,0 +1,18 @@
+﻿let rec generateFibs a b max = 
+    if a + b < max then
+        let current = a + b
+        let rest = generateFibs b current max
+        current :: rest
+    else
+        []
+let fibonacci = 1::2::generateFibs 1 2 4000000
+
+let sumOfEvens list = 
+    list 
+    |> List.filter(fun x -> x % 2 = 0)
+    |> List.sum
+
+let answer = sumOfEvens fibonacci
+printfn "%A" answer
+
+        
