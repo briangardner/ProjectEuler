f8668ddf7bdda5791a71ed3f29c4e06a2be46229
Brian Gardner <brian.gardner@socraticdev.com>
2016-03-05 21:47:36 -0500

    Solved problems 3 and 4.

---
+++ ProjectEuler/Problem3.fsx
@@ -0,0 +1,25 @@
+﻿open System
+
+let getUpperBound number = int64(Math.Sqrt(double(number)))
+
+let isFactor number factorCandidate =
+    number % factorCandidate = 0L
+
+let factors number = seq {
+    for i in [2L ..getUpperBound number] do
+        if isFactor number i then yield i 
+    }
+
+let isPrime number =
+    factors number |> Seq.length = 0
+
+let maxPrime number = 
+    [2L .. getUpperBound number]
+    |> Seq.filter (fun x -> isFactor number x)
+    |> Seq.filter isPrime
+    |> Seq.max
+
+let max = 
+    maxPrime 600851475143L
+
+printfn "%A" max
---
+++ ProjectEuler/Problem4.fsx
@@ -0,0 +1,26 @@
+﻿open System
+let rec isPalindrome (value:string) =
+   let length = value.Length
+   match value with
+    | "" -> true
+    |x -> match length with
+            | 1 -> true
+            | _ -> x.[0] = x.[length - 1] && isPalindrome (x.Substring(1,(length - 2)))
+  
+let maxPalindrome = 
+    let palindromes = seq {
+        for i = 999 downto 100 do
+            for j = 999 downto 100 do
+                let numberString = Convert.ToString(i*j)
+                if isPalindrome(numberString) then
+                    yield i*j
+            
+    }
+    Seq.max palindromes
+
+printf "%A" maxPalindrome
+    
+
+
+
+
