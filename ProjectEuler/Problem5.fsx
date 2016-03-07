3573eb333fe85f08a1e613aae5d3bca76a18b408
Brian Gardner <brian.gardner@socraticdev.com>
2016-03-06 20:37:48 -0500

    Solved Problem 5.

---
+++ ProjectEuler/Problem5.fsx
@@ -0,0 +1,14 @@
+﻿let isFactor(number, divisor) = number % divisor = 0
+
+let areAllFactors (number, numbers) = 
+    numbers |> Seq.forall (fun n -> isFactor(number, n))
+
+let smallestMultipleForAll numbers =
+    let max = Seq.max numbers
+    Seq.unfold (fun start -> Some(start, start + max)) max
+        |> Seq.filter (fun n -> areAllFactors (n, numbers))
+        |> Seq.head
+
+let result = smallestMultipleForAll [1..20]
+
+printfn "%A" result
