99b95b397a275d0d6b76c60e226b40f4bd712a65
Brian Gardner <brian.gardner@socraticdev.com>
2016-03-08 10:04:01 -0500

    Solutions to problems 6 & 7.

---
+++ ProjectEuler/Problem6.fsx
@@ -0,0 +1,17 @@
+﻿let square x = x*x
+
+let sumOfSquares numbers = 
+    let min = Seq.min numbers
+    let max = Seq.max numbers
+    Seq.unfold (fun x -> if (x > max) then None else Some(square x, x+1)) min
+        |> Seq.sum
+
+let squareOfSums numbers =
+    numbers
+        |> Seq.sum
+        |> square
+
+let result =
+    abs (sumOfSquares [1..100] - squareOfSums [1..100])
+
+printfn "%A" result
---
+++ ProjectEuler/Problem7.fsx
@@ -0,0 +1,25 @@
+﻿open System
+
+let getUpperBound number = int64(Math.Sqrt(double(number)))
+
+let isFactor number factorCandidate =
+    number % factorCandidate = 0L
+
+let factors number = seq {
+    for i in [2L ..getUpperBound number] do
+        if isFactor number i then yield i 
+    }
+
+let isPrime number =
+    factors number |> Seq.length = 0
+
+let findNthPrime position =
+    Seq.initInfinite (fun x -> int64 x)
+        |> Seq.filter isPrime
+        |> Seq.item (position+1)
+
+let result = findNthPrime 10001
+
+printfn "%A" result 
+
+
